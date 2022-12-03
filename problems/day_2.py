8584af9ce1034f0f90ae2729ee709dd39eeee68b
Carter Bastian <carter.bastian1@gmail.com>
2022-12-02 16:51:05 -0800

    Day 2 Part 2

--- problems/day_2.py
+++ problems/day_2.py
@@ -31,3 +31,28 @@ def part_1():
             cur_sum += me
 
     return cur_sum
+
+def part_2():
+    input_arr = get_strings_by_lines('2.txt')
+
+    cur_sum = 0
+    for val in input_arr:
+        them = symbol_map[val[0]]
+        match_result = val[2]
+
+        if match_result == "X":
+            # We need a loss
+            me = (them + 3 - 1) % 3
+            me += (3 if me == 0 else 0)
+            cur_sum += me if me > 0 else 3
+        elif match_result == "Y":
+            # We need a Draw
+            me = them
+            cur_sum += them + 3
+        else:
+            # We need a win
+            me = (them + 1) % 3
+            me += (3 if me == 0 else 0)
+            cur_sum += me + 6
+
+    return cur_sum
