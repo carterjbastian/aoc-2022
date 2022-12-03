5c5c79626c943bf709fcc412ee2b14bdc4e5f028
Carter Bastian <carter.bastian1@gmail.com>
2022-12-02 16:29:50 -0800

    Day 2 part 1

---
+++ problems/day_2.py
@@ -0,0 +1,33 @@
+from lib.helpers import log, get_strings_by_lines
+
+symbol_map = {
+    # rocks
+    "A": 1,
+    "X": 1,
+    # papers
+    "B": 2,
+    "Y": 2,
+    # scizzors
+    "C": 3,
+    "Z": 3,
+}
+
+def part_1():
+    input_arr = get_strings_by_lines('2.txt')
+
+    cur_sum = 0
+    for val in input_arr:
+        them = symbol_map[val[0]]
+        me = symbol_map[val[2]]
+
+        if them == me:
+            # Draw
+            cur_sum += me + 3
+        elif (them % 3) + 1 == me:
+            # Win - modular arithmetic just works sometimes don't ask questions
+            cur_sum += me + 6
+        else:
+            # Loss
+            cur_sum += me
+
+    return cur_sum
